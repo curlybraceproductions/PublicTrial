4554e3ecc1de0b99ef1fccbe398c7d06f0743881
TJ <TJ@home-karen>
2012-03-30 20:46:15 -0600

    Added main constructor for all commands

--- PublicTrial/src/com/cbp/PublicTrial/TrialCommand.java
+++ PublicTrial/src/com/cbp/PublicTrial/TrialCommand.java
@@ -1,5 +1,32 @@
 package com.cbp.PublicTrial;
 
-public class TrialCommand {
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class TrialCommand implements CommandExecutor{
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+		if(cmd.getName().equalsIgnoreCase("Trial")){
+			Player p = (Player) sender;
+			if(args.length== 0){
+				
+			}else if(args.length==1){
+				if(p.getServer().getPlayer(args[0]) != null){
+					Player targetPlayer = Bukkit.getPlayer(args[0]);
+				}
+				
+			}else if(args[0].equalsIgnoreCase("Convict")){
+				
+			}else if(args[1].equalsIgnoreCase("Jail")){
+				
+			}else if(args[2].equalsIgnoreCase("Free")){
+				
+			}
+		}
+		return false;
+		
+	}
 
 }
